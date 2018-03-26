7c94f8a9c1579371e2e2d753fbcfc39ebce7afd2
IPa Chen <ipapamagic@gmail.com>
2018-03-26 15:10:32 +0800

    create Designable UI for VisualEffectView

---
+++ IPaDesignableUI/Classes/IPaDesignableVisualEffectView.swift
@@ -0,0 +1,35 @@
+//
+//  IPaDesignableVisualEffectView.swift
+//  IPaDesignableUI
+//
+//  Created by IPa Chen on 2018/3/17.
+//
+
+import UIKit
+
+open class IPaDesignableVisualEffectView: UIVisualEffectView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBInspectable open var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable open var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable open var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
