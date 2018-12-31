c38656cfb200645ca5b2b1bb1cc30e7320e019d2
IPa Chen <ipapamagic@gmail.com>
2018-12-31 15:19:10 +0800

    implement IPadesignableStackView

---
+++ IPaDesignableUI/Classes/IPaDesignableStackView.swift
@@ -0,0 +1,78 @@
+//
+//  IPaDesignableStackView.swift
+//  IPaDesignableUI
+//
+//  Created by IPa Chen on 2018/12/31.
+//
+
+import UIKit
+
+open class IPaDesignableStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBInspectable open var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable open var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable open var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    @IBInspectable open var shadowColor: UIColor? {
+        didSet {
+            self.layer.shadowColor = shadowColor?.cgColor
+        }
+    }
+    @IBInspectable open var shadowRadius: CGFloat {
+        set {
+            self.layer.shadowRadius = newValue
+        }
+        get {
+            return self.layer.shadowRadius
+        }
+    }
+    @IBInspectable open var shadowOffset: CGSize {
+        set {
+            self.layer.shadowOffset = newValue
+        }
+        get {
+            return self.layer.shadowOffset
+        }
+    }
+    @IBInspectable open var shadowOpacity: CGFloat {
+        set {
+            self.layer.shadowOpacity = Float(newValue)
+        }
+        get {
+            return CGFloat(self.layer.shadowOpacity)
+        }
+    }
+    open var shadowPath: CGPath? {
+        set {
+            self.layer.shadowPath = newValue
+        }
+        get {
+            return self.layer.shadowPath
+        }
+    }
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    public required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
