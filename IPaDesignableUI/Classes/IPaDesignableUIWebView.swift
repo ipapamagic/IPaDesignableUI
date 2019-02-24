9b8b9f9b46a7292e8e9ccb163076509d82030338
IPa Chen <ipapamagic@gmail.com>
2019-02-24 21:46:27 +0800

    implement IPaDesignableUIWebView for UIWebView

---
+++ IPaDesignableUI/Classes/IPaDesignableUIWebView.swift
@@ -0,0 +1,107 @@
+//
+//  IPaDesignableUIWebView.swift
+//  IPaDesignableUI
+//
+//  Created by IPa Chen on 2019/2/24.
+//
+
+import UIKit
+
+class IPaDesignableUIWebView: UIWebView {
+
+    @IBInspectable open var isScrollEnabled: Bool {
+        get {
+            return self.scrollView.isScrollEnabled
+        }
+        set  {
+            self.scrollView.isScrollEnabled = newValue
+        }
+    }
+    @IBInspectable open var bounces: Bool {
+        get {
+            return self.scrollView.bounces
+        }
+        set  {
+            self.scrollView.bounces = newValue
+        }
+    }
+    override open var bounds: CGRect {
+        didSet {
+            if self.cornerRadius > 0 && self.shadowColor != nil {
+                self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.cornerRadius).cgPath
+            }
+        }
+    }
+    @IBInspectable open var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            self.layer.masksToBounds = true
+        }
+    }
+    @IBInspectable open var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable open var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    @IBInspectable open var shadowColor: UIColor? {
+        didSet {
+            if shadowColor != nil {
+                self.layer.shadowColor = shadowColor?.cgColor
+                self.clipsToBounds = false
+                self.layer.masksToBounds = false
+                if self.cornerRadius > 0 {
+                    self.layer.shouldRasterize = true
+                    self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.cornerRadius).cgPath
+                }
+                
+            }
+            else {
+                self.layer.shadowColor = nil
+            }
+            
+        }
+    }
+    @IBInspectable open var shadowRadius: CGFloat {
+        set {
+            self.layer.shadowRadius = newValue
+        }
+        get {
+            return self.layer.shadowRadius
+        }
+    }
+    @IBInspectable open var shadowOffset: CGSize {
+        set {
+            self.layer.shadowOffset = newValue
+        }
+        get {
+            return self.layer.shadowOffset
+        }
+    }
+    @IBInspectable open var shadowOpacity: CGFloat {
+        set {
+            self.layer.shadowOpacity = Float(newValue)
+        }
+        get {
+            return CGFloat(self.layer.shadowOpacity)
+        }
+    }
+    open var shadowPath: CGPath? {
+        set {
+            self.layer.shadowPath = newValue
+        }
+        get {
+            return self.layer.shadowPath
+        }
+    }
+    override open var intrinsicContentSize: CGSize {
+        if self.scrollView.isScrollEnabled {
+            return super.intrinsicContentSize
+        }
+        return self.scrollView.contentSize
+    }
+}
